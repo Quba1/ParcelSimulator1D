13e345e2ca3ba6d676e43f1b473640d9fc019700
Jakub Lewandowski <jak.lewand@gmail.com>
2021-01-24 14:46:36 +0100

    Dynamics scheme separation and constants update.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: build/thermo.o build/environment.o build/parcel.o build/pseudo.o  build/dynamic.o | output
+all: build/thermo.o build/environment.o build/parcel.o build/pseudo.o build/RK_dynamic.o build/FD_dynamic.o | output
 	g++ -O3 build/dynamic.o build/pseudo.o build/environment.o build/thermo.o build/parcel.o src/main.cpp -o simulator.exe
 	rm -rf build
 
@@ -14,8 +14,11 @@ build/parcel.o: src/parcel.cpp src/parcel.h | build
 build/pseudo.o: src/pseudoadiabatic_scheme.cpp src/pseudoadiabatic_scheme.h | build
 	g++ -O3 -c src/pseudoadiabatic_scheme.cpp -o build/pseudo.o
 	
-build/dynamic.o: src/dynamic_scheme.cpp src/dynamic_scheme.h | build
-	g++ -O3 -c src/dynamic_scheme.cpp -o build/dynamic.o
+build/RK_dynamic.o: src/runge_kutta_dynamics.cpp src/dynamic_scheme.h | build
+	g++ -O3 -c src/runge_kutta_dynamics.cpp -o build/RK_dynamic.o
+
+build/FD_dynamic.o: src/finite_difference_dynamics.cpp src/dynamic_scheme.h | build
+	g++ -O3 -c src/finite_difference_dynamics.cpp -o build/FD_dynamic.o
 
 build:
 	mkdir build
@@ -26,4 +29,3 @@ output:
 clean:
 	rm -rf build
 	rm -f *.exe
-
--- src/thermodynamic_calc.h
+++ src/thermodynamic_calc.h
@@ -1,14 +1,23 @@
 #ifndef THERMODYNAMICS_H
 #define THERMODYNAMICS_H
 
-#include <cmath>
-
-double const EPSILON = 0.62194572947593730; //ratio of g/mol over g/mol; non SI, but all formulae are adapted to it
+//externaly defined constants
 double const G = 9.80665; //gravitational acceleration in m*s^-2
-double const C_P = 1005.7; //J kg^-1 K^-1 specific heat capacity of dry air at constant pressure
+double const R = 8.31446261815324; //J K^-1 mol^-1 universal gas constant
+
+double const M_D = 0.028966; // kg mol^-1 molar mass of dry air (Gatley, 2008)
+double const M_V = 0.0180152833; //kg mol^-1 molar mass of water vapour
+
+double const C_P = 1005.7; //J kg^-1 K^-1 specific heat capacity of dry air at constant pressure (Bolton, 1980)
 double const C_V = 718; //J kg^-1 K^-1 specific heat capacity of dry air at constant volume
-double const C_PV = 1870; //J kg^-1 K^-1 specific heat capacity of water vapour at constant pressure
+double const C_PV = 1875; //J kg^-1 K^-1 specific heat capacity of water vapour at constant pressure (Bolton, 1980)
 double const C_VV = 1410; //J kg^-1 K^-1 specific heat capacity of water vapour at constant volume
+
+double const L_V = 2501000; //J kg^1 latent heat of vapourization of water at 0C (Bolton, 1980)
+
+//pre-calculated constants
+double const EPSILON = M_V / M_D; //ratio of molar masses of dry air and water vapour
+double const R_D = R / M_D; //specific gas constant for dry air
 
 double calcMixingRatio(double temperature, double pressure);
 
