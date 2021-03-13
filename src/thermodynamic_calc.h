620734d82639d4592f8e4bb9e525492a9656720d
Jakub Lewandowski <jak.lewand@gmail.com>
2021-03-13 21:36:31 +0100

    updated constants using values from ECMWF IFS documentation

--- src/thermodynamic_calc.h
+++ src/thermodynamic_calc.h
@@ -5,15 +5,15 @@
 double const G = 9.80665; //gravitational acceleration in m*s^-2
 double const R = 8.31446261815324; //J K^-1 mol^-1 universal gas constant
 
-double const M_D = 0.028966; // kg mol^-1 molar mass of dry air (Gatley, 2008)
+double const M_D = 0.0289644; // kg mol^-1 molar mass of dry air (ECMWF, 2020)
 double const M_V = 0.0180152833; //kg mol^-1 molar mass of water vapour
 
-double const C_P = 1005.7; //J kg^-1 K^-1 specific heat capacity of dry air at constant pressure (Bolton, 1980)
-double const C_V = 718; //J kg^-1 K^-1 specific heat capacity of dry air at constant volume
-double const C_PV = 1875; //J kg^-1 K^-1 specific heat capacity of water vapour at constant pressure (Bolton, 1980)
-double const C_VV = 1410; //J kg^-1 K^-1 specific heat capacity of water vapour at constant volume
+double const C_P = 1004.709; //J kg^-1 K^-1 specific heat capacity of dry air at constant pressure (ECMWF, 2020)
+double const C_V = 717.6493; //J kg^-1 K^-1 specific heat capacity of dry air at constant volume (ECMWF, 2020)
+double const C_PV = 1846.1; //J kg^-1 K^-1 specific heat capacity of water vapour at constant pressure (ECMWF, 2020)
+double const C_VV = 1384.575; //J kg^-1 K^-1 specific heat capacity of water vapour at constant volume (ECMWF, 2020)
 
-double const L_V = 2501000; //J kg^1 latent heat of vapourization of water at 0C (Bolton, 1980)
+double const L_V = 2500800.0; //J kg^1 latent heat of vapourization of water (ECMWF, 2020)
 
 //pre-calculated constants
 double const EPSILON = M_V / M_D; //ratio of molar masses of dry air and water vapour
